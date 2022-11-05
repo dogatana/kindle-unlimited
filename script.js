28724ed8218598171946a86dd91b025f88b62cdd
Toshihiko Ichida <dogatana@gmail.com>
2022-11-05 10:06:59 +0900

    ローカルファイルとして動作＆BS5利用

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
  * kindle unlimited検索
  *
  */
-function search()
-{
-  var word = document.getElementById('word').value;
-  var word_enc = encodeURIComponent(word);
-  var url = 'https://www.amazon.co.jp/s?rh=n%3A2250738051%2Ck%3A' + word_enc + '%2Cp_n_feature_nineteen_browse-bin%3A3169286051'
+function search() {
+  const word = document.getElementById('word').value;
+  const word_enc = encodeURIComponent(word);
+  const url = 'https://www.amazon.co.jp/s?rh=n%3A2250738051%2Ck%3A' + word_enc + '%2Cp_n_feature_nineteen_browse-bin%3A3169286051'
   window.open(url, '_blank');
 }
 
